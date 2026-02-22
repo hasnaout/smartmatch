a03142a2daa52f23377f13b61db84d0affb09172
Hasna <hasn.outaleb@edu.umi.ac.ma>
2026-02-22 16:42:48 +0000

    creation de fichier de controller de connexion et inscription client ,prestataire, admin

---
+++ backend/controllers/userController.js
@@ -0,0 +1,16 @@
+//Route pour User connecxion
+const connectUser=async(req,res)=>{
+
+}
+
+//Route pour User inscription
+const inscrirUser=async(req,res)=>{
+
+}
+
+//Route pour Admin connecxion
+const connectAdmin=async(req,res)=>{
+
+}
+
+export {connectUser,inscrirUser,connectAdmin}
