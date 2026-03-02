074b5d1f75fa75393ca2f7904b7115f0433ca673
Hasna <hasn.outaleb@edu.umi.ac.ma>
2026-03-02 22:25:20 +0000

    ajout de true pour required , et changement de date_creation par timestamps

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,14 +1,17 @@
 import mongoose from 'mongoose';
 const {Schema}=mongoose;
 const userSchema=new Schema({
-  nom:{ type:String ,required},
-  prenom:{type:String,required},
-  email:{type:String,required,unique:true},
-  adresse:{type:String,required},
-  telephone:{type:String,required},
-  password:{type:String,required},
+  nom:{ type:String ,required:true},
+  prenom:{type:String,required:true},
+  email:{type:String,required:true,unique:true},
+  adresse:{type:String,required:true},
+  telephone:{type:String,required:false},
+  password:{type:String,required:true},
+  img:{type:String,required:false},
+  desc:{type:String,required:false},
   role:{type:String,enum:["client","prestataire","admin"],default:"client"},
-  date_creation:{type:Date,default:Date.now},
+},{
+  timestamps:true
 });
 const User=mongoose.models.User || mongoose.model("User",userSchema);
 /*si model deja existe on l'utilise , sinon le crée */
