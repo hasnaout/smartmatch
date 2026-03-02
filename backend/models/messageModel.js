3ddf850111a6a6b251751f04bb2631ea58e527bc
Hasna <hasn.outaleb@edu.umi.ac.ma>
2026-03-02 23:12:50 +0000

    message schema

---
+++ backend/models/messageModel.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const {Schema} =mongoose;
+const MessageSchema=new Schema({
+  conversationId:{type:String,required:true},
+  userId:{type:String,required:true},
+  desc:{type:String,required:true},
+},{
+    timestamps:true
+});
+const Message=mongoose.models.Message || mongoose.model("Message",DemandeMessage);
+export default Message;
