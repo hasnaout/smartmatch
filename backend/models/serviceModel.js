b35a0a69338b0f658b0388d9d9425f9ad4504422
Hasna <hasn.outaleb@edu.umi.ac.ma>
2026-03-02 22:45:57 +0000

    ajout des champs au shcema service

--- backend/models/serviceModel.js
+++ backend/models/serviceModel.js
@@ -2,10 +2,23 @@ import mongoose from "mongoose";
 
 const {Schema} =mongoose;
 const ServiceSchema=new Schema({
-  userid:{type:String,required},
-  title:{type:String,required},
-  totalStars:{type:Number,required},
-  starNumber:{type:Number,required},
-  cat:{type:String,required},
-  prix:{type:Number,required},
-})
+  userid:{type:String,required:true},
+  titre:{type:String,required:true},
+  desc:{type:String,required:true},
+  totalStars:{type:Number,default:0},
+  starNumber:{type:Number,required:true},
+  categorie:{type:String,required:true},
+  prix:{type:Number,required:true},
+  cover:{type:String,required:true},
+  images:{type:[String],required:false},
+  shortTitre:{type:String,required:true},
+  shortDesc:{type:String,required:true},
+  date_pub:{type:Number,required:true},
+  revisions:{type:Number,required:true},
+  features:{type:[String],required:false},
+},{
+    timestamps:true
+});
+const Service=mongoose.models.Service || mongoose.model("Service",ServiceSchema);
+/*si model deja existe on l'utilise , sinon le crée */
+export default Service;
