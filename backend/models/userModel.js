1b6d79c2e335397fb759a41f0fedb6b1dacda1f6
Hasna <hasn.outaleb@edu.umi.ac.ma>
2026-03-02 22:02:43 +0000

    ajout de quelque champ dans user schema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,13 +1,15 @@
 import mongoose from 'mongoose';
-
-const userSchema=new mongoose.Schema({
+const {Schema}=mongoose;
+const userSchema=new Schema({
   nom:{ type:String ,required:true},
+  prenom:{type:String,required:true},
   email:{type:String,required:true,unique:true},
+  adresse:{type:String,required:true},
+  telephone:{type:String,required:true},
   password:{type:String,required:true},
   role:{type:String,enum:["client","prestataire","admin"],default:"client"},
-  date_creation:{type:Date,default:Date.now}
+  date_creation:{type:Date,default:Date.now},
 });
 const User=mongoose.models.User || mongoose.model("User",userSchema);
 /*si model deja existe on l'utilise , sinon le crée */
-
 export default User;
