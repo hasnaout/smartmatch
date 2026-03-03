321f99181eadbde9199a72ed47155f5cce86c4e4
Hasna <hasn.outaleb@edu.umi.ac.ma>
2026-03-03 01:04:18 +0000

    ajout de midlware cookieParser

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,18 @@ import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/coudinary.js';
 import userRouter from './routes/userRoute.js';
 import authRoute from './routes/authRoute.js';
+import cookieParser from "cookie-parser";
 
 //App config 
-const app=express()
+const app=express();
 const port=process.env.PORT  || 4000
 connectDB()
 connectCloudinary()
 
 //Middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
 
 //api endpoints
