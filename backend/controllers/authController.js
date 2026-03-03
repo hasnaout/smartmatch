946bfcb6ac5d3987f33e078ff509246f6d3ac3fc
Hasna <hasn.outaleb@edu.umi.ac.ma>
2026-03-03 13:50:13 +0000

    ajout de token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const inscription=async(req,res)=>{
   try {
@@ -23,8 +24,18 @@ export const connexion=async (req,res)=>{
   const isCorrect=bcrypt.compareSync(req.body.password,user.password);
   if(!isCorrect) return res.status(400).send("mot de passe incorrect");
 
+  const token =jwt.connexion({
+    id:user._id,
+    estPresataire:user.estPresataire,
+  },
+  process.env.JWT_SECRET
+);
   const {password,...infos}=user._doc
-  res.status(200).send(infos)
+  res.cookie("accessToken",token,{
+    httpOnly:true,
+  })
+  .status(200)
+  .send(infos)
  } catch (error) {
    res.status(500).send("il y a un probleme")
  }
