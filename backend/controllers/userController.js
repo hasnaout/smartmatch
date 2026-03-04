03302393d875d40dec7598ba5e3ff219ce6ecb95
Hasna <hasn.outaleb@edu.umi.ac.ma>
2026-03-04 16:55:58 +0000

    nv modif simple

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,32 +2,29 @@ import User from "../models/userModel.js";
 import jwt from 'jsonwebtoken';
 export const deleteUser=async (req,res)=>{
  try {
-  const token=req.cookies.accessToken;
-  if(!token) return res.status(401).json({
-      success: false,
-      message: "Vous n'êtes pas authentifié"
-  });
-
-  const decode=jwt.verify(token,process.env.JWT_SECRET);
-
-  if(decode.id!==req.params.id && decode.role !=="admin"){
+  if(req.user.id!==req.params.id && req.user.role !=="admin"){
     return res.status(403).json({
       success:false,
       message:"Accès refusé"
     });
   }
+
   const user=await User.findByIdAndDelete(req.params.id)
+
   if(!user){
     return res.status(404).json({
       success:false,
       message:"Utilisateur introuvable"
     });
   }
+
   res.clearCookie("accessToken");
+
   res.status(200).json({
     success:true,
     message:"Compte supprimé avec succés"
   });
+  
  } catch (error) {
     res.status(500).json({
       success:false,
