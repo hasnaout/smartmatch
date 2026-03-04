899a2e9cf249b179102f982b5b80268ba0fc5c7f
Hasna <hasn.outaleb@edu.umi.ac.ma>
2026-03-04 15:27:57 +0000

    new update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,37 @@
 import User from "../models/userModel.js";
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 export const deleteUser=async (req,res)=>{
-
-  const user=await User.findById(req.params.id)
+ try {
   const token=req.cookies.accessToken;
-  if(!token) return res.status(401).send("vous etes pas authentifier");
-  jwt.verify(token,process.env.JWT_SECRET,(err,payload)=>{
-     if(!payload.id===user._id){
-      return res.status(403).send("Vous pouvez seuelemnt supprimer votre compte")
-     }
-  })
-  await User.findBYIdandDelete(req.params.id);
-  res.status(200).send("supprimer avec succées")
-}
+  if(!token) return res.status(401).json({
+      success: false,
+      message: "Vous n'êtes pas authentifié"
+  });
+
+  const decode=jwt.verify(token,process.env.JWT_SECRET);
+
+  if(decode.id!==req.params.id && decode.role !=="admin"){
+    return res.status(403).json({
+      success:false,
+      message:"Accès refusé"
+    });
+  }
+  const user=await User.findByIdAndDelete(req.params.id)
+  if(!user){
+    return res.status(404).json({
+      success:false,
+      message:"Utilisateur introuvable"
+    });
+  }
+  res.clearCookie("accessToken");
+  res.status(200).json({
+    success:true,
+    message:"Compte supprimé avec succés"
+  });
+ } catch (error) {
+    res.status(500).json({
+      success:false,
+      message:"Erreur serveur"
+    });
+ }
+};
